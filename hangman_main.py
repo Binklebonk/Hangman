1c09a7ac9e250889ddc1c583816fa74c2ea6fedc
Noah <noah.dodgshun@gmail.com>
2026-01-24 10:32:55 +1300

    Added comments
    Because I forgot

--- hangman_main.py
+++ hangman_main.py
@@ -2,28 +2,34 @@
 # By Noah Dodgshun
 # 32/01/2026
 
+# Set word to guess and amount of guesses
 word_to_guess = 'hello'
 turns = 5
 
 def game(word_to_guess, turns):
+    # Setting up the display of what letters you have guessed
     guessed_word = ['_'] * len(word_to_guess)
     guessed_letters = []
-    for i in range(turns):
+
+    for i in range(turns): # Repeating this for the amount of turns you have
+        # Printing out turn info
         print(f'\nYou have {turns} turns left.')
         print(''.join(guessed_word))
         guess = input('Enter a letter to guess: ')
 
-        if guess in guessed_letters:
+        if guess in guessed_letters: # Making sure you can't guess the same letter twice
             print('You already guessed that.')
         else:
-            guessed_letters.append(guess)
+            guessed_letters.append(guess) # Adding letter to guessed letter list
 
+            # Loop to replace the guessed letter as many times as it appears in the word
             digit = 0
             for i in word_to_guess:
                 if word_to_guess[digit] == guess:
                     guessed_word[digit] = guessed_word[digit].replace('_', i)
                 digit +=1
 
+            # Win condition
             if guessed_word == word_to_guess:
                 print(f'{guessed_word}\nYou win!')
 
