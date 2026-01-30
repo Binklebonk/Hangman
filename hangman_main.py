1ff10b157f6e3f29a2e6491b477f09e8d33c8dd8
Noah <noah.dodgshun@gmail.com>
2026-01-31 10:29:41 +1300

    Scoring change and word guessing fix
    Altered try calculation and changed the way that guessing the whole word is handled

--- hangman_main.py
+++ hangman_main.py
@@ -84,11 +84,12 @@ def game(word_to_guess):
     guessed_word = ['_ '] * len(word_to_guess) # Setting up display of letters guessed
     all_letters = string.ascii_lowercase # All letters string to show what letters haven't been guessed
     win = False # Win condition to exit loop
+    bad_guess = False
     turn_message = 'Good luck!' # Setting up turn message
 
     # Calculate number of tries the user has depending on word length
-    tries = len(word_to_guess)
-    if len(word_to_guess) > 10: # No more than 10 tries max
+    tries = len(word_to_guess) + 1
+    if len(word_to_guess) > 10: # No more than 12 tries max
         tries = 10
 
     while True: # Repeating this for the amount of tries you have
@@ -113,25 +114,29 @@ def game(word_to_guess):
                 break
 
         if len(guess) > 1: # Making sure you can't accidentally type more than one letter
-            if guess == word_to_guess: # If the user guesses the whole word
-                win = True
-                add_to_leaderboard = ynbox(
-                    'You guessed the word! Good job.\n'
-                    'Do you want to add your score to the leaderboard?',
-                    'Win'
-                )
-                if not add_to_leaderboard:
-                    break
-                if add_to_leaderboard:
-                    # Setting difficulty, as it is not stored at any point
-                    if len(word_to_guess) < 7:
-                        difficulty = 'Easy'
-                    elif len(word_to_guess) >= 7 and len(word_to_guess) < 10:
-                        difficulty = 'Medium'
-                    else:
-                        difficulty = 'Hard'
-                    leaderboard(write = True, difficulty = difficulty, tries = tries)
-                    break
+            if len(guess) == len(word_to_guess):
+                if guess == word_to_guess: # If the user guesses the whole word
+                    win = True
+                    add_to_leaderboard = ynbox(
+                        'You guessed the word! Good job.\n'
+                        'Do you want to add your score to the leaderboard?',
+                        'Win'
+                    )
+                    if not add_to_leaderboard:
+                        break
+                    if add_to_leaderboard:
+                        # Setting difficulty, as it is not stored at any point
+                        if len(word_to_guess) < 7:
+                            difficulty = 'Easy'
+                        elif len(word_to_guess) >= 7 and len(word_to_guess) < 10:
+                            difficulty = 'Medium'
+                        else:
+                            difficulty = 'Hard'
+                        leaderboard(write = True, difficulty = difficulty, tries = tries)
+                        break
+                else:
+                    bad_guess = True
+                    tries = 0
             else:
                 turn_message = 'Make sure your answer is only one letter.'
         else:
@@ -140,6 +145,7 @@ def game(word_to_guess):
                     turn_message = 'Please enter a letter.'
                 else:
                     turn_message = 'You already guessed that.'
+                    tries -= 1
             else:
                 all_letters = all_letters.replace(guess, '_ ') # Removing letter from letter list
 
@@ -179,8 +185,10 @@ def game(word_to_guess):
         if tries == 0: break # Exit loop when you run out of tries
 
     # Lose
-    if win == False:
+    if win == False and bad_guess == False:
         msgbox(f'You lose!\nThe word was {word_to_guess}.')
+    elif win == False and bad_guess == True:
+        msgbox(f'You guessed the word incorrect. Better luck next time!\nThe word was {word_to_guess}.')
 
 def main(word_to_guess):
     while True: # Loop to repeat back to this box once leaderboard is closed
@@ -189,7 +197,8 @@ def main(word_to_guess):
             'You will be able to select a difficulty, which will change the amount of letters in the word.\n\n'
             'To guess, type a letter in the input box. You will be able to see what letters you have guessed,'
             'and the amount of correct letters you already have. It is not case sensitive.\n\n'
-            'If you think you know what the finished word is, then type it in and guess.\n\n'
+            'If you think you know what the finished word is, then type it in and guess.'
+            'But be careful, you only have one try! If you guess a word and it\'s wrong, game over.\n\n'
             'The amount of tries you have is matched to the length of the word up to 10 tries.'
             'Tries will only go down for guessing incorrectly.\n\n'
             'If you manage to guess your word, you win!\nGood luck!',
